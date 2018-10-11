935f19e1cb6f31939ed2cb3657a9510430d797fc
Jacob Tang <wyspchip@gmail.com>
2018-10-11 12:28:16 -0700

    Completed tasks 3-5

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ class Main
     
     public static void main( String[] args)
     {
+        
      
      
         
@@ -33,29 +34,51 @@ class Main
         int[] ar1 = new int[n];
         
         for (int i=0; i<n; i++)
-            ar1[i]=n-i;
+            ar1[i]=i;
             
         for (int i=0; i<n; i++)
             System.out.println("ar1["+i+"] = "+ar1[i]);
                 
         /*
-         * Task 2.  Create a new array called ar2.
-         *          Copy the elements of ar1 into ar2.
-         */
-        System.out.println("**ar2**");
-        int[]ar2 = new int[ar1.length];
-        
-        //int[] ar2 = ar1.clone();
-        for (int i=0; i<ar2.length; i++)
-            ar2[i]=i;
-        
-        for (int i=0; i<ar2.length; i++)
-            System.out.println("ar2["+i+"] = "+ar2[i]);
+        * Task 2. Create a new array called ar2.
+        * Copy the elements of ar1 into ar2.
+        */
+ 
+       System.out.println("** ar2 **");
+ 
+       int[] ar2;
+ 
+       // 
+       // ar2 = ar1.clone() also does this
+       // ar2 = ar1, just copies the address so you are not copying the array
+       //
+       ar2= new int[ ar1.length ]; // sets the size of the array to equal ar1.
+       for (int i=0 ; i<ar2.length ; i++)
+       {
+           ar2[i]=ar1[i];
+        }
+ 
+        // ar2[ i ] = i 
+       for (int i=0 ; i<ar2.length ; i++)
+           System.out.println("ar2[" + i + "] = "+ar2[i]);
+        // ar2[ i ] = i
+ 
+        // Just copies the memory location so ar2 and ar1 are literally the same
+        // ar2=ar1
         
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
         
+        System.out.println("**Task 3**");
+        
+        for (int i=0; i<ar1.length; i++) {
+            ar1[i] = ar1[i]+1;
+        }
+        
+        for (int i=0; i<ar1.length; i++) {
+            System.out.println("ar1["+i+"] = "+ar1[i]);
+        }
 
         
         
@@ -66,12 +89,38 @@ class Main
          *         ar1: 1 2 3
          *         ar3: 1 2 3 0 1 2 3
          */
+        System.out.println("**Task 4**");
+        
+        int[] ar3 = new int[2*ar1.length];
+        
+        for (int i=0; i<ar1.length; i++)
+            ar3[i] = ar1[i];
+        for (int i=0; i<ar1.length; i++)
+            ar3[i+ar1.length] = ar1[i];
+        for (int i=0; i<ar3.length; i++)
+            System.out.println("ar3["+i+"] = "+ar3[i]);
+         
         
         
         /*
          * Task 5.  Switch the first and last element of ar1.
          *          print out the new ar1.  Then switch them back
          */
+        System.out.println("**Task 5**");
+        
+        int value;
+        value = ar1[0];
+        ar1[0] = ar1[ar1.length-1];
+        ar1[ar1.length-1] = value;
+        
+        for (int i=0; i<ar1.length; i++)
+            System.out.println("ar1["+i+"] = "+ar1[i]);
+        
+        //Restore
+        value = ar1[0];
+        ar1[0] = ar1[ar1.length-1];
+        ar1[ar1.length-1] = value;
+        
         
         /*
          * Task 6A. Print the 2nd to (n-1)th elements of ar1
